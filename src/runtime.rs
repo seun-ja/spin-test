bc2246c253374bf02530dd129353a26300fd2bb0
Aminu 'Seun Joshua <seun.aminujoshua@gmail.com>
2025-07-29 14:13:17 +0100

    - respects component exclude files Signed-off-by: Aminu 'Seun Joshua <seun.aminujoshua@gmail.com>
    - fmt
    Signed-off-by: Aminu 'Seun Joshua <seun.aminujoshua@gmail.com>
    
    - cleaner code
    Signed-off-by: Aminu 'Seun Joshua <seun.aminujoshua@gmail.com>

--- src/runtime.rs
+++ src/runtime.rs
@@ -88,7 +88,6 @@ impl Runtime {
 
     /// Make all mounted files visible to the WASI virtual filesystem
     fn add_files(&mut self, runner: dynamic::DynamicRunner) -> anyhow::Result<()> {
-        //TODO(rylev): handle component.exclude_files
         /// Make a file visible to the WASI virtual filesystem
         fn add_file<T>(
             store: &mut wasmtime::Store<T>,
@@ -116,6 +115,15 @@ impl Runtime {
 
                         // Host path is the absolute path to the file
                         let host_path = self.manifest.absolute_from(host_path);
+                        // If the file among list of files to be excluded, skip it
+                        if self
+                            .manifest
+                            .component()
+                            .exclude_files
+                            .contains(&host_path.display().to_string())
+                        {
+                            continue;
+                        }
                         // Only add files
                         if !host_path.is_file() {
                             continue;
@@ -140,6 +148,15 @@ impl Runtime {
                             .unwrap_or(destination.as_str())
                     );
                     let host_path = self.manifest.absolute_from(source);
+                    // If the file among list of files to be excluded, skip it
+                    if self
+                        .manifest
+                        .component()
+                        .exclude_files
+                        .contains(&host_path.display().to_string())
+                    {
+                        continue;
+                    }
 
                     // If the host path is a directory, add all files in the directory
                     if host_path.is_dir() {
